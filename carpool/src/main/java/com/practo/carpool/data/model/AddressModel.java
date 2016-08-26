6d0a116bb6c26284ab4dcb4712b9e78da9a9ce99
Aditya Bhatia <aditya_bhatia52@yahoo.co.in>
2016-08-26 10:06:45 +0530

    removed extra columns from addressModel

--- carpool/src/main/java/com/practo/carpool/data/model/AddressModel.java
+++ carpool/src/main/java/com/practo/carpool/data/model/AddressModel.java
@@ -5,46 +5,23 @@ package com.practo.carpool.data.model;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
-import java.util.Date;
 
 import com.practo.carpool.data.entity.Address;
+import com.practo.carpool.exceptions.NotFoundException;
 
 /**
  * @author aditya
  *
  */
 public class AddressModel implements Serializable {
+  
   private static final long serialVersionUID = 1L;
 
   private int id;
 
-  private byte active;
-
-  private String administrativeAreaLevel1;
-
-  private String administrativeAreaLevel2;
-
-  private String country;
-
-  private Date createdAt;
-
-  private Date deletedAt;
-
   private BigDecimal latitude;
 
   private BigDecimal longitude;
-
-  private Date modifiedAt;
-
-  private String neighbourhood;
-
-  private String postalCode;
-
-  private String route;
-
-  private String streetNumber;
-
-  private String sublocality;
 
   public AddressModel() {}
 
@@ -54,63 +31,15 @@ public class AddressModel implements Serializable {
   }
 
   public int getId() {
-    return this.id;
+    return id;
   }
 
   public void setId(int id) {
     this.id = id;
   }
 
-  public byte getActive() {
-    return this.active;
-  }
-
-  public void setActive(byte active) {
-    this.active = active;
-  }
-
-  public String getAdministrativeAreaLevel1() {
-    return this.administrativeAreaLevel1;
-  }
-
-  public void setAdministrativeAreaLevel1(String administrativeAreaLevel1) {
-    this.administrativeAreaLevel1 = administrativeAreaLevel1;
-  }
-
-  public String getAdministrativeAreaLevel2() {
-    return this.administrativeAreaLevel2;
-  }
-
-  public void setAdministrativeAreaLevel2(String administrativeAreaLevel2) {
-    this.administrativeAreaLevel2 = administrativeAreaLevel2;
-  }
-
-  public String getCountry() {
-    return this.country;
-  }
-
-  public void setCountry(String country) {
-    this.country = country;
-  }
-
-  public Date getCreatedAt() {
-    return this.createdAt;
-  }
-
-  public void setCreatedAt(Date createdAt) {
-    this.createdAt = createdAt;
-  }
-
-  public Date getDeletedAt() {
-    return this.deletedAt;
-  }
-
-  public void setDeletedAt(Date deletedAt) {
-    this.deletedAt = deletedAt;
-  }
-
   public BigDecimal getLatitude() {
-    return this.latitude;
+    return latitude;
   }
 
   public void setLatitude(BigDecimal latitude) {
@@ -118,59 +47,11 @@ public class AddressModel implements Serializable {
   }
 
   public BigDecimal getLongitude() {
-    return this.longitude;
+    return longitude;
   }
 
   public void setLongitude(BigDecimal longitude) {
     this.longitude = longitude;
-  }
-
-  public Date getModifiedAt() {
-    return this.modifiedAt;
-  }
-
-  public void setModifiedAt(Date modifiedAt) {
-    this.modifiedAt = modifiedAt;
-  }
-
-  public String getNeighbourhood() {
-    return this.neighbourhood;
-  }
-
-  public void setNeighbourhood(String neighbourhood) {
-    this.neighbourhood = neighbourhood;
-  }
-
-  public String getPostalCode() {
-    return this.postalCode;
-  }
-
-  public void setPostalCode(String postalCode) {
-    this.postalCode = postalCode;
-  }
-
-  public String getRoute() {
-    return this.route;
-  }
-
-  public void setRoute(String route) {
-    this.route = route;
-  }
-
-  public String getStreetNumber() {
-    return this.streetNumber;
-  }
-
-  public void setStreetNumber(String streetNumber) {
-    this.streetNumber = streetNumber;
-  }
-
-  public String getSublocality() {
-    return this.sublocality;
-  }
-
-  public void setSublocality(String sublocality) {
-    this.sublocality = sublocality;
   }
 
   // model to entity
@@ -184,11 +65,13 @@ public class AddressModel implements Serializable {
   }
 
   // entity to model
-  public void entityPost(Address addressEntity) {
-    if (addressEntity != null) {
-      setLongitude(addressEntity.getLongitude());
+  public void entityPost(Address addressEntity) throws NotFoundException {
+    if (addressEntity != null && addressEntity.getDeletedAt() == null) {
+      setId(addressEntity.getId());
       setLatitude(addressEntity.getLatitude());
+      setLongitude(addressEntity.getLongitude());
+    } else {
+      throw new NotFoundException("Address with given id doesn't exist");
     }
-
   }
 }
