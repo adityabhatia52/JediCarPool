5c2efa5ecfa4bf3c6d42d3b0a087825ef7f622bc
Aditya Bhatia <aditya_bhatia52@yahoo.co.in>
2016-08-26 10:12:45 +0530

    integrate hibernate

--- carpool/src/main/java/com/practo/carpool/run/Application.java
+++ carpool/src/main/java/com/practo/carpool/run/Application.java
@@ -8,12 +8,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 /**
  * @author aditya
  *
  */
 @SpringBootApplication
+@ImportResource("classpath:hibernate.xml")
 @ComponentScan(basePackages = {"com.practo.carpool"})
 @EnableJpaRepositories(basePackages = {"com.practo.carpool.repository"})
 @EntityScan("com.practo.carpool.data.entity")
