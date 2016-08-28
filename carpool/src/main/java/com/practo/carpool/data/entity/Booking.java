eb901672d98571a27a418719ebe2b203e6a76bce
Aditya Bhatia <aditya_bhatia52@yahoo.co.in>
2016-08-28 14:24:11 +0530

    Changed Id generation scheme from AUTO to IDENTITY

--- carpool/src/main/java/com/practo/carpool/data/entity/Booking.java
+++ carpool/src/main/java/com/practo/carpool/data/entity/Booking.java
@@ -15,7 +15,7 @@ public class Booking implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int idBooking;
 
 	@Temporal(TemporalType.TIMESTAMP)
